6be8520d15fd3f96e8382729514498abb8100fbb
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-01-31 18:38:25 -0500

    in-progress, currently server 500s

--- download.rb
+++ download.rb
@@ -8,34 +8,30 @@
 
 require 'curb'
 require 'json'
+require 'pry-byebug'
 
 @folder_name = folder_name = 'ggtracker-replays'
-@download_tracking_file_name = 'downloaded.replays'
 puts 'replays will be saved in the "' + folder_name + '" folder'
 
-def download_replay(id)
-  puts "downloading replay: #{id}"
-  url = "http://ggtracker.com/matches/#{id}/replay"
+def download_replay(url)
+  puts "downloading replay from #{url}"
 
-  Curl::Easy.download(url, filename = "#{@folder_name}/#{id}.SC2Replay")
-
-  open(@download_tracking_file_name, 'a') { |f|
-    f.puts id.to_s
-  }
+  Curl::Easy.download(url, filename = "#{@folder_name}/#{url.split(/\?/).first.split(/\//).last}")
 end
 
-def get_all_replay_ids_for_identity(identity = 4914, limit_to = 700)
+def get_all_replay_s3_links_for_identity(identity = 4914, limit_to = 700)
   puts 'downloading replay data from ggtracker...'
-  url = "http://api.ggtracker.com/api/v1/matches?category=Ladder&game_type=1v1&identity_id=#{identity}&replay=true&limit=#{limit_to}"
+  url = "http://api.ggtracker.com/api/v1/matches?category=Ladder&limit=#{limit_to}&game_type=1v1&identity_id=#{identity}&replay=true&filter=-graphs,match(replays,-map,-map_url),entity(-summary,-minutes,-armies)"
 
+  puts 'making a request to ' + url
   c = Curl::Easy.perform(url)
   json = JSON.parse(c.body_str)
   replays = json['collection']
-  ids = replays.map do |replay_info|
-    replay_info['id']
+  s3_liks = replays.map do |replay_info|
+    replay_info['replays'].map{|r| r['url'] }
   end
 
-  ids
+  s3_links.flatten
 end
 
 
@@ -44,24 +40,14 @@ unless File.directory?(folder_name)
   Dir.mkdir folder_name
 end
 
-already_downloaded = []
-if File.exists?(@download_tracking_file_name)
-  File.open(@download_tracking_file_name) do |f|
-    f.each_line.each do |line|
-      already_downloaded << line.strip
-    end
-  end
-end
-
-replay_ids = get_all_replay_ids_for_identity.map(&:to_s)
-replay_ids = replay_ids - already_downloaded
-if replay_ids.count > 0
-  puts "there are #{replay_ids.count} to download..."
-  replay_ids.each do |id|
-    download_replay(id)
+links = get_all_replay_s3_links_for_identity
+if links.count > 0
+  puts "there are #{links.count} to download..."
+  links.each do |url|
+    download_replay(url)
   end
 
   puts 'done!'
 else
-  puts "you've already downloaded all your replays. if you wish to re-download them, delete the downloaded.replay file"
+  puts "there are noreplays to download"
 end

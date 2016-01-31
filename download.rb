0f0474753226e372978d7ddcef2bc9c8176df862
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-01-31 14:05:08 -0500

    added a little something to allow resuming of downloads, in case of interruption

--- download.rb
+++ download.rb
@@ -10,6 +10,7 @@ require 'curb'
 require 'json'
 
 @folder_name = folder_name = 'ggtracker-replays'
+@download_tracking_file_name = 'downloaded.replays'
 puts 'replays will be saved in the "' + folder_name + '" folder'
 
 def download_replay(id)
@@ -17,6 +18,10 @@ def download_replay(id)
   url = "http://ggtracker.com/matches/#{id}/replay"
 
   Curl::Easy.download(url, filename = "#{@folder_name}/#{id}.SC2Replay")
+
+  open(@download_tracking_file_name, 'a') { |f|
+    f.puts id.to_s
+  }
 end
 
 def get_all_replay_ids_for_identity(identity = 4914, limit_to = 700)
@@ -39,9 +44,24 @@ unless File.directory?(folder_name)
   Dir.mkdir folder_name
 end
 
-replay_ids = get_all_replay_ids_for_identity
-replay_ids.each do |id|
-  download_replay(id)
+already_downloaded = []
+if File.exists?(@download_tracking_file_name)
+  File.open(@download_tracking_file_name) do |f|
+    f.each_line.each do |line|
+      already_downloaded << line.strip
+    end
+  end
 end
 
-puts 'done!'
+replay_ids = get_all_replay_ids_for_identity.map(&:to_s)
+replay_ids = replay_ids - already_downloaded
+if replay_ids.count > 0
+  puts "there are #{replay_ids.count} to download..."
+  replay_ids.each do |id|
+    download_replay(id)
+  end
+
+  puts 'done!'
+else
+  puts "you've already downloaded all your replays. if you wish to re-download them, delete the downloaded.replay file"
+end
